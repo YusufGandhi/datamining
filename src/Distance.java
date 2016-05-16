06445412697925f0bba856ce1f21c4c5f7a773a8
Yusuf Gandhi Putra <u5705518@anu.edu.au>
2016-05-16 14:56:49 +1000

    Distance cleaned

--- src/Distance.java
+++ src/Distance.java
@@ -1,38 +1,40 @@
+/** File: Distance.java **/
 import java.math.BigDecimal;
-
 
 public class Distance {
 	public enum Func {
+		// the Euclidean distance representation and distance calculation function
 		EUCLIDEAN {
 			public double getDistance(DataPoint obs1, DataPoint obs2) {
 				if(!IsCompatible(obs1,obs2)) throw new UnsupportedOperationException("Observations don't match");
 				double distance = 0.0;
-				
-//				System.out.println(obs1.getFeatures());
-//				System.out.println(obs2.getFeatures());
 				
 				for (Integer i : obs1.getNumIndex()) {
 					distance += ((Double) obs1.getFeatures().get(i) - (Double) obs2.getFeatures().get(i)) *
 							((Double) obs1.getFeatures().get(i) - (Double) obs2.getFeatures().get(i));
 				}
 				return new BigDecimal(Math.sqrt(distance)).setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
-			}
-		}, MANHATTAN {
+			}	
+		},
+		// the Manhattan distance representation and the distance calculation function
+		MANHATTAN {
 			public double getDistance(DataPoint obs1, DataPoint obs2) {
 				if(!IsCompatible(obs1,obs2)) throw new UnsupportedOperationException("Observations don't match");
 				double distance = 0.0;
 				
-				// only iterating through the numeric index
 				for (Integer i : obs1.getNumIndex()) {
 					distance += Math.abs((Double) obs1.getFeatures().get(i) - (Double) obs2.getFeatures().get(i));
 				}
 				return distance;
 			}
-		}, COSINE {
+		}, 
+		// the Cosine distance representation and the distance calculation
+		COSINE {
 			public double getDistance(DataPoint obs1, DataPoint obs2) {
 				if(!IsCompatible(obs1,obs2)) throw new UnsupportedOperationException("Observations don't match");
 				double result = dotProduct(obs1, obs2) / 
 						(obs1.getMagnitude() * obs2.getMagnitude());
+				// error handling: return 1.0 when it gets infinite result
 				if(Double.isInfinite(result) || Double.isNaN(result)) return 1.0;
 				return new BigDecimal(1.0 - result).setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
 			}
@@ -41,14 +43,7 @@ public class Distance {
 		abstract double getDistance(DataPoint obs1, DataPoint obs2);
 	}
 	
-	/**
-	 * Dot product of a two DataPoint is derived from this reference:
-	 * https://en.wikipedia.org/wiki/Euclidean_vector#Dot_product
-	 * The function returns the dot product of two DataPoint objects
-	 * @param obs1 the first DataPoint object
-	 * @param obs2 the second DataPoint object
-	 * @return     the dot product of the first and second DataPoint objects
-	 */
+	// function to calculate the dot product of two vectors
 	private final static double dotProduct(DataPoint obs1, DataPoint obs2) {
 		if(!IsCompatible(obs1,obs2)) throw new UnsupportedOperationException("Observations don't match");
 		
@@ -59,13 +54,7 @@ public class Distance {
 		return sum;
 	}
 	
-	/**
-	 * The function checks whether two DataPoiint objects are compatible
-	 * (read: have the dimension and the same type of features for each dimension)
-	 * @param o1 the first DataPoint object
-	 * @param o2 the second DataPoint object
-	 * @return   true if both DataPoints have the same features (length and type); false otherwise
-	 */
+	// function to check whether the two data points are compatible
 	private static boolean IsCompatible(DataPoint o1, DataPoint o2) {
 		if(o1.featureSize() != o2.featureSize()) return false;
 
