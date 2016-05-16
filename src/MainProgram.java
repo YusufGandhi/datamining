eefc8556e6099b5121eb5bf80ccff60a491e0590
Yusuf Gandhi Putra <u5705518@anu.edu.au>
2016-05-16 17:37:41 +1000

    Adding the number of iterations and others

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -6,11 +6,16 @@ public class MainProgram {
 		CSV csv = new CSV("forestfires.normalized.final.csv"); // the CSV file
 		// KMeans initialization
 		KMeans km = new KMeans(csv /* the CSV file */, 
-								2  /* number of centroids */, 
+								6  /* number of centroids */, 
 								Distance.Func.EUCLIDEAN /* distance function*/, 
 								100 /* max iteration */, 
 								Centroid.Init.RANDOM/* centroid initialization function */);
 		
 		ArrayList<Centroid> centroids = km.runClustering();
+		int i = 1;
+		for(Centroid c : centroids) {
+			System.out.println("Centroid #" + i++ + ": " +c);
+		}
+		System.out.println("Iteration: " + km.getIteration());
 	}
 } /** End of MainProgram.java **/
